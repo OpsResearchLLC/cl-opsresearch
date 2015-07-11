3796c60acc999f24af1ea9cb924b9d9f99d72582
Donald Anderson <dranderson@opsresearch.com>
2015-07-10 20:26:57 -0400

    fixed duplicate names in gsl

--- subsystem/or-gsl/swig/swig.sh
+++ subsystem/or-gsl/swig/swig.sh
@@ -2,5 +2,10 @@
 
 swig -cffi interface.swig 
 echo '(in-package #:gsl)' > gsl-fixed.lisp
-cat gsl.lisp >> gsl-fixed.lisp
+cat gsl.lisp \
+	| sed 's/swig-lispify "gsl_sf_bessel_J/swig-lispify "gsl_sf_bessel_jj/' \
+	| sed 's/swig-lispify "gsl_sf_bessel_Y/swig-lispify "gsl_sf_bessel_yy/' \
+	| sed 's/swig-lispify "gsl_sf_bessel_I/swig-lispify "gsl_sf_bessel_ii/' \
+	| sed 's/swig-lispify "gsl_sf_bessel_K/swig-lispify "gsl_sf_bessel_kk/' \
+	>> gsl-fixed.lisp
 mv -f gsl-fixed.lisp gsl.lisp
